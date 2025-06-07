8d95999f2addd8f75d3c9ed4369c635f5b579aab
sanil <sparmar27feb2003@gmail.com>
2025-06-07 09:33:43 +0530

    Initial file uploading and file saving

---
+++ backend/app/api/upload.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, UploadFile, File
+import os
+
+UPLOAD_DIR = "/Users/sanilparmar/Desktop/wasserStoff_chatbot/backend/data"
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "go to http://127.0.0.1:8000/docs!"}
+
+@app.post("/uploadFile/")
+async def upload_file(file: UploadFile = File(...)):
+    file_location = os.path.join(UPLOAD_DIR, file.filename)
+    with open (file_location, "wb") as f:
+        f.write(await file.read())
+    return {"filename": file.filename, "content_type": file.content_type, "Saved succesfully at": file_location}
