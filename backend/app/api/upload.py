9628d3b0a08da170ab59db7ce0f22677ef7bd02a
sanil <sparmar27feb2003@gmail.com>
2025-06-11 07:36:50 +0530

    Routing of api, for upload and searching.

--- backend/app/api/upload.py
+++ backend/app/api/upload.py
@@ -10,8 +10,8 @@ async def root():
     return {"message": "go to http://127.0.0.1:8000/docs!"}
 
 @app.post("/uploadFile/")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(file: UploadFile = File()):
     file_location = os.path.join(UPLOAD_DIR, file.filename)
     with open (file_location, "wb") as f:
-        f.write(await file.read())
-    return {"filename": file.filename, "content_type": file.content_type, "Saved succesfully at": file_location}
+        f.write(file.read())
+    print(f"filename: {file.filename}, content_type: {file.content_type}, Saved succesfully at: {file_location}")
